0b0a430db2b63b3563e3cdd470cbff71b6a031d0
770881781 <770881781@qq.com>
2018-02-01 10:26:13 +0800

    20180201

---
+++ db_oa.php
@@ -0,0 +1,12 @@
+<?php
+require 'myPDO.class.php';
+$db_oa = myPDO::getInstance('127.0.0.1:3306', 'root', 'trust___MeILIU@!#@$@bEIjING','isp_oa', 'utf8');
+$re_oa = $db_oa->query("SELECT `name`,`coordinate` FROM `ioa_school` WHERE `coordinate` <> 0 ");
+foreach ($re_oa as $key => $value) {
+    $coordinate = explode(',',$value['coordinate']);
+    $res[$value['name']][0] = $coordinate[0];
+    $res[$value['name']][1] = $coordinate[1];    
+    $res_val[$key]['name'] = $value['name'];
+    $res_val[$key]['value'] = rand(99,999);
+}unset($db_oa);unset($re_oa);
+?>
---
+++ db_sns.php
@@ -0,0 +1,23 @@
+<?php
+$db_sns = myPDO::getInstance('127.0.0.1:3306', 'root', 'trust___MeILIU@!#@$@bEIjING','isp_sns', 'utf8');
+$res_sns = $db_sns->query("select a.school_id,b.name,count(a.id) as peop_num from isns_user_class a, isns_ioa_school b where  a.school_id = b.school_id group by school_id");
+$timestart = mktime(-24,0,0, date('m'), date('d'), date('Y'));        
+$timesend  = mktime(0,0,0, date('m'), date('d'), date('Y'));    
+foreach ($res_sns as $key => $value) {
+    $school_id = $value['school_id'];
+    $peop = $value['peop_num'];
+    $number =  $db_sns->query("SELECT `user_id` FROM `isns_action_log` WHERE isns_action_log.create_time BETWEEN $timestart AND $timesend AND isns_action_log.action_id = 3 AND isns_action_log.school_id = $school_id GROUP BY isns_action_log.user_id");  
+    $number = count($number);
+    $res_sns[$key]['number'] = $number;
+    $res_sns[$key]['number_is'] = (number_format($number/$peop,2))*100;
+}
+$newArr=array();
+$newArr_p=array();
+for($j=0;$j<count($res_sns);$j++){
+    $newArr[]=$res_sns[$j]['number_is'];
+    $newArr_p[]=$res_sns[$j]['peop_num'];
+}
+array_multisort($newArr,SORT_DESC,$newArr_p,SORT_DESC, $res_sns);
+$res_json = json_encode($res_sns);
+unset($db_sns);unset($newArr);unset($newArr_p);unset($res_sns);
+?>
